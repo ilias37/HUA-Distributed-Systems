55b5d8219c9407f4773cabc508cca4a8cea0eb2e
crazy committer <iliaspanag37@outlook.com>
2023-08-25 14:42:18 +0300

    allagmeno asstets/db

--- assets/db/users.sql
+++ assets/db/users.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS authorities;
-DROP TABLE IF EXISTS users;
-
-
+--DROP TABLE IF EXISTS authorities;
+--DROP TABLE IF EXISTS users;
+--
+--
 CREATE TABLE IF NOT exists users (
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(100) NOT NULL,
@@ -38,6 +38,18 @@ INSERT INTO authorities(username, authority) VALUES
 
 
 INSERT INTO actors(username, firstname, lastname, email) VALUES
+    ('actor1', 'Actor1', 'One1', 'actor1@hua.gr'),
+    ('actor2', 'Actor2', 'Two2', 'actor2@hua.gr')
+;    
+
+
+INSERT INTO notaries(username, firstname, lastname, email) VALUES
+    ('notary1', 'Notary1', 'One1', 'notary1@hua.gr'),
+    ('notary2', 'Notary2', 'Two2', 'notary2@hua.gr')
+;    
+
+
+INSERT INTO actors(username, firstname, lastname, email) VALUES
     ('john', 'John', 'Ajohn', 'john@hua.gr'),
     ('nick', 'Nick', 'Anick', 'nick@hua.gr'),
     ('pavlos', 'Pavlos', 'Apavlos', 'pavlos@hua.gr'),
@@ -57,3 +69,77 @@ INSERT INTO contracts(notaryid, sellerid, buyerid, address, tax, sellerdec, buye
     (2, 1, 3, 'Dramas 34', 200, false, false),
     (3, 2, 4, 'Ioanninwn 56', 300, false, false)
 ;
+
+
+INSERT INTO userreg (username, firstname, lastname, email, password, role) VALUES
+    ('actor1', 'Actor1', 'One1', 'actor1@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_ACTOR'),
+    ('actor2', 'Actor2', 'Two2', 'actor2@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_ACTOR'),
+    ('john', 'John', 'Ajohn', 'john@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_ACTOR'),
+    ('nick', 'Nick', 'Anick', 'nick@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_ACTOR'),
+    ('pavlos', 'Pavlos', 'Apavlos', 'pavlos@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_ACTOR'),
+    ('mike', 'Mike', 'Amike', 'mike@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_ACTOR'),
+    ('notary1', 'Notary1', 'One1', 'notary1@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_NOTARY'),
+    ('notary2', 'Notary2', 'Two2', 'notary2@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_NOTARY'),
+    ('symbol1', 'Symbol1', 'Asymbol1', 'symbol1@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_NOTARY'),
+    ('symbol2', 'Symbol2', 'Asymbol2', 'symbol2@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_NOTARY'),
+    ('symbol3', 'Symbol3', 'Asymbol3', 'symbol3@hua.gr', '$2a$04$DR/f..s1siWJc8Xg3eJgpeB28a4V6kYpnkMPeOuq4rLQ42mJUYFGC', 'ROLE_NOTARY')
+;
+
+
+-------------------------------------------------------------------------------------------
+
+--create table actors (
+--    id  serial not null,
+--    email varchar(50),
+--    firstName varchar(30),
+--    lastName varchar(30),
+--    username varchar(255),
+--	primary key (id)
+--);
+--
+--    
+--create table contracts (
+--    id  serial not null,
+--    address varchar(30),
+--    buyerdec boolean,
+--    buyerId int4 not null,
+--    notaryId int4 not null,
+--    sellerdec boolean,
+--    sellerId int4 not null,
+--    tax int4 not null,
+--    primary key (id)
+--);
+--
+--    
+--create table notaries (
+--   id  serial not null,
+--    email varchar(50),
+--    firstName varchar(30),
+--    lastName varchar(30),
+--    username varchar(255),
+--    primary key (id)
+--);
+--
+--    
+--alter table actors drop constraint UK_dqey1tbodurhtadt040er82k1;
+--
+--    
+--alter table actors add constraint UK_dqey1tbodurhtadt040er82k1 unique (email);
+--
+--       
+--alter table actors drop constraint UK_syugrn88hpahn8ypmrwnvwkd4;
+--
+--    
+--alter table actors add constraint UK_syugrn88hpahn8ypmrwnvwkd4 unique (username);
+--
+--    
+--alter table notaries drop constraint UK_d4sfwehfheox3uibhic99o2rp;
+-- 
+--    
+--alter table notaries add constraint UK_d4sfwehfheox3uibhic99o2rp unique (email);
+--
+--    
+--alter table notaries drop constraint UK_s7wrm7vf8w19213bkwkr6kyie;
+--
+--    
+--alter table notaries add constraint UK_s7wrm7vf8w19213bkwkr6kyie unique (username);
